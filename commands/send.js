7b305ae7189a8f4511a59306e0613c7c2b3bbf12
mistermemer <77989086+mistermemer@users.noreply.github.com>
2022-09-10 08:14:03 -0700

    s

--- commands/send.js
+++ commands/send.js
@@ -5,12 +5,10 @@ let role = await message.guild.roles.cache.get(args[0]);
 if(!role) return message.reply(`You must provide a valid role ID`);
 if(!args[1]) return message.reply(`You must provide a message which should be sent.`);
 let msg = args.splice(1, args.length);
-await role.members.forEach(async(x) => {
-    try{
-    await x.send(`Important Message: \n\n\`\`\`\n${msg}\n\`\`\``)
-    }catch(e){
-        console.log(e)
-    }
+await role.members.forEach((x) => {
+
+x.send(`Important Message: \n\n\`\`\`\n${msg}\n\`\`\``).catch(() => message.reply(`Can't send DMs to ${x.user.username}`));
+
 })
 message.reply(`Sent this message to every user.`)
 
